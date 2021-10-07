0eb092d3d0d2ef6d8d806e433b459a8f5235692b
qedk <1994constant@gmail.com>
2021-10-07 19:30:23 +0530

    Add and configure linter

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -7,9 +7,6 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 
-// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-const INFURA_ID = process.env.INFURA_ID; //"460f40a260564ac4a4f4b3fffb032dad";
-const PORT = process.env.PORT || 8080;
 /// 📡 What chain are your contracts deployed to?
 
 // const targetNetwork = {
