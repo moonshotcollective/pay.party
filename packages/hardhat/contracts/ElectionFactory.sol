0e3444a160c2c991d041dd761e2fb2eec72072dc
Hans Martin <hansmrtn@pm.me>
2021-10-14 17:19:28 -0700

    updated: contract bugfix

--- packages/hardhat/contracts/ElectionFactory.sol
+++ packages/hardhat/contracts/ElectionFactory.sol
@@ -46,7 +46,7 @@ contract ElectionFactory is Distributor {
 
     modifier onlyElectionAdmin(string memory electionId) {
         require(
-            isElectionAdmin[electionId][msg.sender],
+            !isElectionAdmin[electionId][msg.sender],
             "Sender Not Election Admin!"
         );
         _;
