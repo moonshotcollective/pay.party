749f41cf487310bea073ac0f1e034dcabd04919a
Cali93 <armut.cumaali@gmail.com>
2021-10-15 04:08:09 +0200

    mainnet abi

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -100,7 +100,6 @@ module.exports = {
 
     rinkeby: {
       url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
-
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
