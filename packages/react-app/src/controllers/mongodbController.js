0617d26d1a14239e9e1ce4d0e6ba08ced080c3dd
hans <hmrtn@pm.me>
2021-12-17 16:40:10 -0800

    added: api url

--- packages/react-app/src/controllers/mongodbController.js
+++ packages/react-app/src/controllers/mongodbController.js
@@ -3,19 +3,19 @@ import HttpController from "./httpController";
 export default class MongoDBController {
   http = new HttpController();
   async newParty(data) {
-    const url = "http://localhost:8080/party";
+    const url = `${process.env.REACT_APP_API_URL}/party`;
     return this.http.post(url, {}, data); // Clean this up
   }
   async fetchAllParties() {
-    const url = "http://localhost:8080/party";
+    const url = `${process.env.REACT_APP_API_URL}/party`;
     return this.http.get(url);
   }
   async fetchParty(id) {
-    const url = `http://localhost:8080/party/${id}`;
+    const url = `${process.env.REACT_APP_API_URL}/party/${id}`;
     return this.http.get(url);
   }
   async updateParty(id, data) {
-    const url = `http://localhost:8080/party/${id}`;
+    const url = `${process.env.REACT_APP_API_URL}/party/${id}`;
     return this.http.put(url, data);
   }
 }
