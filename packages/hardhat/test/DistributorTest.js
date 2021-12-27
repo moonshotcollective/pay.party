80aaf11421e7e2155e6d0a9ed2d8c7386f884513
hans <hmrtn@pm.me>
2021-12-27 12:30:05 -0800

    test updates

--- packages/hardhat/test/DistributorTest.js
+++ packages/hardhat/test/DistributorTest.js
@@ -8,8 +8,7 @@ use(solidity);
 
 describe("Pay Party Test", function () {
   let Distributor;
-  // TODO: Test tokens
-  //   let TestToken;
+  let Token;
 
   // quick fix to let gas reporter fetch data from gas station & coinmarketcap
   before((done) => {
@@ -19,7 +18,6 @@ describe("Pay Party Test", function () {
   describe("Distributor Contract", () => {
     it("Should deploy Distributor", async () => {
       const DistributorFactory = await ethers.getContractFactory("Distributor");
-
       Distributor = await DistributorFactory.deploy();
     });
 
@@ -41,22 +39,38 @@ describe("Pay Party Test", function () {
       });
     });
 
-    // it("Should distribute tokens", async () => {
-    //   let token = TestToken.address;
-    //   console.log(TestToken);
-    //   const recipients = [
-    //     "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-    //     "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
-    //     "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
-    //     "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
-    //   ];
-    //   const amounts = [
-    //     BigNumber.from(toWei("1")),
-    //     BigNumber.from(toWei("1")),
-    //     BigNumber.from(toWei("1")),
-    //     BigNumber.from(toWei("1")),
-    //   ];
-    //   await Distributor.distributeToken(token, recipients, amounts);
-    // });
+    it("Should deploy Test Token", async () => {
+      const TokenFactory = await ethers.getContractFactory("Token");
+      const sender = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; //await ethers.getSigners()[0];
+      Token = await TokenFactory.deploy(sender, "TOKEN", "TST");
+    });
+
+    it("Should get approved Token allowance", async () => {
+      const signer = await ethers.getSigners()[0];
+      await Token.approve(
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        toWei("500")
+      );
+    });
+
+    it("Should distribute tokens", async () => {
+      let tokenAdr = Token.address;
+      const recipients = [
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
+        "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
+        "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
+      ];
+      const amounts = [
+        BigNumber.from(toWei("1")),
+        BigNumber.from(toWei("1")),
+        BigNumber.from(toWei("1")),
+        BigNumber.from(toWei("1")),
+      ];
+      // await Distributor.connect("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+      await Distributor.connect(
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+      ).distributeToken(tokenAdr, recipients, amounts);
+    });
   });
 });
