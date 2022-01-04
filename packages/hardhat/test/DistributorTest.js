8da24a258928946db9645fa9d8822004e2b8ebe1
hans <hmrtn@pm.me>
2022-01-03 16:17:48 -0800

    updated: solmate v6 + larger tests

--- packages/hardhat/test/DistributorTest.js
+++ packages/hardhat/test/DistributorTest.js
@@ -34,10 +34,24 @@ describe("Pay Party Test", () => {
         "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
         "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
         "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
+
+        "0xF9E401ca2b801Fad97A0267D7E26693D0c62d99D",
+        "0xf7a5fdccB356C6FC296EE08bc991aE6c69c18ce2",
+        "0x6b541b78349097714B9D1aB6A788dB5e0dCF21a3",
+        "0x802999C71263f7B30927F720CF0AC10A76a0494C",
       ];
-      const amounts = ["0x1000", "0x1000", "0x1000", "0x1000"];
+      const amounts = [
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+      ];
       await Distributor.distributeEther(recipients, amounts, "test", {
-        value: "0x4000",
+        value: "0x8000",
       });
     });
 
@@ -47,9 +61,23 @@ describe("Pay Party Test", () => {
         "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
         "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
         "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
+
+        "0xF9E401ca2b801Fad97A0267D7E26693D0c62d99D",
+        "0xf7a5fdccB356C6FC296EE08bc991aE6c69c18ce2",
+        "0x6b541b78349097714B9D1aB6A788dB5e0dCF21a3",
+        "0x802999C71263f7B30927F720CF0AC10A76a0494C",
       ];
-      const amounts = ["0x1000", "0x1000", "0x1000", "0x1000"];
-      await Token.approve(Distributor.deployTransaction.creates, "0x4000");
+      const amounts = [
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+        "0x1000",
+      ];
+      await Token.approve(Distributor.deployTransaction.creates, "0x8000");
       await Distributor.distributeToken(
         Token.address,
         recipients,
