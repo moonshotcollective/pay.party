b8803e1af8f07188111e6c73a3dda91d41139a52
hans <hmrtn@pm.me>
2022-01-05 11:42:28 -0800

    updated env vars

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,11 +1,8 @@
-// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "bd19f29ceeb04aeb83009866f836474b"; //"460f40a260564ac4a4f4b3fffb032dad";
+require("dotenv").config();
 
-// MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
-
-// BLOCKNATIVE ID FOR Notify.js:
-export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
+export const INFURA_ID = process.env.REACT_APP_INFURA_ID;
+export const ETHERSCAN_KEY = process.env.REACT_APP_ETHERSCAN_KEY;
+export const BLOCKNATIVE_DAPPID = process.env.REACT_APP_BLOCKNATIVE_DAPPID;
 
 export const NETWORKS = {
   localhost: {
