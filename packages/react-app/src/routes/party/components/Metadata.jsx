98a02b3bd618f4d925d263c867029fe1030b8adb
bhargavaparoksham <aparokshambhargav@gmail.com>
2022-03-08 15:02:03 +0530

    feat: link to faq

--- packages/react-app/src/routes/party/components/Metadata.jsx
+++ packages/react-app/src/routes/party/components/Metadata.jsx
@@ -1,5 +1,5 @@
-import { useToast, Box, Button, Text, Center, Tooltip, Input, Tag, TagLabel, TagRightIcon } from "@chakra-ui/react";
-import { QuestionOutlineIcon, CopyIcon } from "@chakra-ui/icons";
+import { useToast, Box, Button, Text, Center, Tooltip, Input, Tag, TagLabel, TagRightIcon, Link } from "@chakra-ui/react";
+import { QuestionOutlineIcon, CopyIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
 
 export const Metadata = ({ partyData, mainnetProvider, votesData, distribution, strategy }) => {
@@ -14,15 +14,6 @@ export const Metadata = ({ partyData, mainnetProvider, votesData, distribution, 
       </Center>
       <Center pt={4}>
         <Text fontSize="xl">{`${partyData.name}`}</Text>
-      </Center>
-      <Center pt={4} pl="5%" pr="5%">
-        <Text fontSize="xs">{`Signature: ${partyData.signed.signature.substr(
-          0,
-          6,
-        )}...${partyData.signed.signature.substr(
-          partyData.signed.signature.length - 4,
-          partyData.signed.signature.length,
-        )}`}</Text>
       </Center>
       <Center pt={4}>
         <Tooltip label={cpText}>
@@ -40,6 +31,13 @@ export const Metadata = ({ partyData, mainnetProvider, votesData, distribution, 
       </Center>
       <Center pt={4} pl="5%" pr="5%">
         <Text fontSize="sm">{`${partyData.description}`}</Text>
+      </Center>
+      <Center pt={4} pl="5%" pr="5%">
+        <Text>To know more about how voting, voting strategy etc works, check out our  
+          <Link href='https://pay.party' isExternal m="2">
+              FAQ page <ExternalLinkIcon mx='2px' />
+          </Link> 
+        </Text>
       </Center>
       <Center p="4">
         <Text fontWeight="semibold" fontSize="lg">
